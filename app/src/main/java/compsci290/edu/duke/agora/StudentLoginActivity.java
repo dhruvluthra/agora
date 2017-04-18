2715954b7c6a35deb5aaa4d1febebf3b8ea9e06a
gruvydhruvy <git@github.com:gruvydhruvy/>
2017-04-17 21:04:54 -0400

    with login, new home activity

---
+++ app/src/main/java/compsci290/edu/duke/agora/StudentLoginActivity.java
@@ -0,0 +1,17 @@
+package compsci290.edu.duke.agora;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lisasapozhnikov on 4/11/17.
+ */
+
+public class StudentLoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_login);
+    }
+
+}
---
+++ app/src/main/java/compsci290/edu/duke/agora/TeacherLoginActivity.java
@@ -0,0 +1,16 @@
+package compsci290.edu.duke.agora;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lisasapozhnikov on 4/11/17.
+ */
+
+public class TeacherLoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teacher_login);
+    }
+}
